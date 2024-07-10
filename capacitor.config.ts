8a188d56b11839a7c8d1b86975a4eb0ef7996cdd
Felix Olszewski <e12022502@student.tuwien.ac.at>
2024-07-10 21:51:15 +0200

    first

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'write-preferences-web-read-user-defaults-ios',
+  webDir: 'www'
+};
+
+export default config;
